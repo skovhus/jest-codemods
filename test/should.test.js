e72b7660cfed793dbb84d0b95e6f3e2d700688c6
benjamin99 <cknomber1@gmail.com>
2017-03-21 14:30:13 +0800

    added the test case for should.exist

--- test/should.test.js
+++ test/should.test.js
@@ -8,3 +8,17 @@ test.a.b.should.eql('123');
 // original: test.c.should.equals(123)
 test.c.should.equals(123);
 
+test.should.be.a('string');
+
+test.should.equal('bar');
+
+test.should.have.lengthOf(3);
+
+test.should.have.property('tea').with.lengthOf(3);
+
+// original: should.exist(test.a)
+should.exist(test.a);
+
+// original: should.not.exist(test.a.b);
+should.not.exist(test.a.b);
+
