40126d3669a13781fbafdf836692c01e23946510
skovhus <kenneth.skovhus@gmail.com>
2020-06-02 21:49:57 +0200

    Release using script

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+version=$(cat package.json | jq -r .version)
+name=$(cat package.json | jq -r .name)
+
+publishedVersion=$(yarn info "$name" --json | jq -r .data.\"dist-tags\".latest)
+
+if [ "$version" = "$publishedVersion" ]; then
+    echo "Newest version is already deployed."
+    exit 0
+fi
+
+echo "Deploying version $version."
+
+npm publish
