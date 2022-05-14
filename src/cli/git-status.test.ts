213c002d88cd1c95bab7fd0675ea9e137e68e917
skovhus <kenneth.skovhus@gmail.com>
2022-05-14 23:37:38 +0200

    Reset mocks

--- src/cli/git-status.test.ts
+++ src/cli/git-status.test.ts
@@ -13,53 +13,47 @@ jest.setMock('is-git-clean', {
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const checkGitStatus = require('./git-status').default
 
-const _process: any = process
+beforeAll(() => {
+  jest.spyOn(console, 'log').mockImplementation()
+  jest.spyOn(console, 'warn').mockImplementation()
+  jest.spyOn(process, 'exit').mockImplementation()
+})
 
 it('does not exit and output any logs when git repo is clean', () => {
   gitStatusReturnValue = true
-  console.log = jest.fn()
-  _process.exit = jest.fn()
   checkGitStatus()
   expect(console.log).not.toHaveBeenCalled()
-  expect(_process.exit).not.toHaveBeenCalled()
+  expect(process.exit).not.toHaveBeenCalled()
 })
 
 it('does not exit and output any logs when not a git repo', () => {
   const err = new Error() as any
   err.stderr = 'Not a git repository'
   gitStatusReturnValue = err
-  console.log = jest.fn()
-  _process.exit = jest.fn()
   checkGitStatus()
   expect(console.log).not.toHaveBeenCalled()
-  expect(_process.exit).not.toHaveBeenCalled()
+  expect(process.exit).not.toHaveBeenCalled()
 })
 
 it('exits and output logs when git repo is dirty', () => {
   gitStatusReturnValue = false
-  console.log = jest.fn()
-  _process.exit = jest.fn()
   checkGitStatus()
   expect(console.log).toHaveBeenCalled()
-  expect(_process.exit).toHaveBeenCalled()
+  expect(process.exit).toHaveBeenCalled()
 })
 
 it('exits and output logs when git detection fail', () => {
   gitStatusReturnValue = new Error('bum')
-  console.log = jest.fn()
-  _process.exit = jest.fn()
   checkGitStatus()
   expect(console.log).toHaveBeenCalled()
-  expect(_process.exit).toHaveBeenCalled()
+  expect(process.exit).toHaveBeenCalled()
 })
 
 it('does not exit when git repo is dirty and force flag is given', () => {
   gitStatusReturnValue = false
-  console.log = jest.fn()
-  _process.exit = jest.fn()
   checkGitStatus(true)
   expect(console.log).toHaveBeenCalledWith(
     'WARNING: Git directory is not clean. Forcibly continuing.\n'
   )
-  expect(_process.exit).not.toHaveBeenCalled()
+  expect(process.exit).not.toHaveBeenCalled()
 })
